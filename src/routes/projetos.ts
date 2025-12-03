d1080baebbc5a8ebe99c11da94bf08ff0a8b8050
MVHAmorim <marcus@spedpraquemfaz.com.br>
2025-12-02 21:23:49 -0300

    Fix: Secure project routes with auth middleware

--- src/routes/projetos.ts
+++ src/routes/projetos.ts
@@ -1,8 +1,11 @@
 import { Hono } from "hono";
 import { Ambiente, Variaveis } from "../config/ambiente";
 import { ProjetoController } from "../controllers/ProjetoController";
+import { authMiddleware } from "../middleware/auth";
 
 const projetosRoutes = new Hono<{ Bindings: Ambiente, Variables: Variaveis }>();
+
+projetosRoutes.use('*', authMiddleware);
 
 projetosRoutes.get('/', ProjetoController.listar);
 projetosRoutes.post('/', ProjetoController.criar);
